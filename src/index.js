c87be794ca52be89ab15d8317890187d886afaec
Yauheni Haurylenka <djonkaupervud@mail.ru>
2022-02-14 16:46:04 +0300

    feat: add solutiontask

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+  let arrString = str.split('');
+  for (let i = 0; i < arrString.length; i++) {
+    bracketsConfig.forEach((el) => {
+      if (arrString[i] === el[0] && arrString[i + 1] === el[1]) {
+        arrString.splice(i, 2);
+        i = -1;
+      }   
+    })
+  }
+  return !arrString.length;
 }
